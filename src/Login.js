9c48b3918cc1ae261f030e6d273f2430750b7e5e
Afrah Ali <116621717+Afrahbalushi@users.noreply.github.com>
2024-02-01 15:04:16 +0400

    updated

--- src/Login.js
+++ src/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-form">
-        <div className="login-header">Modern Login</div>
+        <div className="login-header">Login Here</div>
         <div className="input-group">
           <label className="input-label" htmlFor="username">
             Username
